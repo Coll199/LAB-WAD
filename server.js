d09384352a83b86d7e8db55d82ac41659b044ba2
Coll199 <feherrob199@gmail.com>
2017-10-10 23:43:13 +0300

    Added User Accounts

--- server.js
+++ server.js
@@ -1,32 +1,66 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const http = require('http');
-const app = express();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-// API file for interacting with MongoDB
-//const api = require('./server/routes/api');
+var mongodbUri = 'mongodb://admin:admin@ds113445.mlab.com:13445/wadproj';
 
-// Parsers
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+//connect to MongoDB
+mongoose.connect(mongodbUri, {
+  useMongoClient: true,
+  socketTimeoutMS: 30000,
+  keepAlive: true,
+  reconnectTries: 30
+});
+var db = mongoose.connection;
 
-// Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// API location
-//app.use('/api', api);
-
-// Send all other requests to the Angular app
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+  console.log('Database connection established');
 });
 
-//Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
 
-const server = http.createServer(app);
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+// serve static files from template
+app.use(express.static(__dirname + '/loginpage'));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+// listen on port 3000
+app.listen(3000, function () {
+  console.log('Express app listening on port 3000');
+});
