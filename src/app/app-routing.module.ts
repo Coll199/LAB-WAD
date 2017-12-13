b7a1001d00a75b027ac3b7269a5f7458116c77d9
Coll199 <feherrob199@gmail.com>
2017-12-13 10:46:44 +0200

    guard game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'leaderboard', component: LeaderboardComponent},
-  { path: 'game', component: SnakeComponent}
+  { path: 'game', component: SnakeComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
